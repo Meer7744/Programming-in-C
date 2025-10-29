ad9fc3fe4c4de6fb5d17f25903d2bf541c43325d
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:09:54 +0530

    Exp1.2.c

---
+++ Exp1.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h> int main(){ printf ("Name- Meer\nSAP_ID-590028247\nCourse-BCA\nBatch-5"); 
+printf("\n  \n"); 
+printf("House No:653 \n"); 
+printf("Street :Sector 3\n"); 
+printf("City:Rudrapur\n"); 
+printf("State :Uttrakhand\n"); 
+printf("Pincode :263145\n");
+return 0; 
+} 
