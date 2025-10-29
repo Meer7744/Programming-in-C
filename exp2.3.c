a319538250e68fa03180c36dff0b24334c79ef30
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:15:35 +0530

    exp2.3.c

---
+++ exp2.3.c
@@ -0,0 +1,12 @@
+#include<stdio.h> #include<math.h> int main(){ 
+float R, N; int P; 
+printf("Name- Meer\nSAP ID - 590028247\nCourse- BCA\nBatch- B5"); 
+	printf("\n 	\n"); 
+printf("Enter the value of P : "); 
+scanf("%d", &P); printf("Enter the value of R : "); 
+  scanf("%f", &R); printf("Enter the value of N : "); 
+  scanf("%f", &N); 
+  float A= P*(pow((1 + R/100), N))- P; 
+  printf("The CI is %f", A); 
+  return 0; 
+} 
