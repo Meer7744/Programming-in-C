3e37b621848ddbdef0e667caaa4a7e8a1a67af8e
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:29:30 +0530

    exp6.3.c

---
+++ exp6.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() {     int num = 1;     int rows = 4;  
+ int i, j;  
+  printf("Name- Meer\nSAP ID - 590028247\nCourse- BCA\nBatch- B5");  
+  printf("\n-----------------\n"); 
+   for(i = 1; i <= rows; i++) {for(j = 1; j <= i; j++) {if (i == 4 && (j == 2 || j == {printf("9  ");  
+} else {printf("%d  ", num); num++;   }  
+ }  
+ printf("\n");  
+ }  
+ return 0;  
+}  
