e2faae8bb7096b8633c454671ff780d39ae2f4f2
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:11:50 +0530

    exp1.4.c

---
+++ exp1.4.c
@@ -0,0 +1,15 @@
+#include <stdio.h> int main() 
+{ 
+printf ("Name-Meer\nSAP_ID-590028247\nCourse-BCA\nBatch-5"); 
+printf("\n  \n"); 
+int a; int b; 
+int SUM; int SUB; int MUL; 
+int DIV; 
+printf("Enter the value of number 1 "); 
+scanf("%d",Ca); 
+printf("Enter the value of number 2 "); scanf("%d",Cb); SUM=a+b; 
+printf("The sum of %d and %d is: %d\n", a, b, SUM); SUB=a-b; 
+printf("The sub of %d and %d is: %d\n", a, b, SUB); MUL=a*b; 
+printf("The mul of %d and %d is: %d\n", a, b, MUL); DIV=a/b; 
+printf("The div of %d and %d is: %d\n", a, b, DIV); return 0; 
+} 
