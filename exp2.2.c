dedb6ff4385794fc46c447f168b51ea47187d006
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:14:43 +0530

    exp2.2.c

---
+++ exp2.2.c
@@ -0,0 +1,9 @@
+#include<stdio.h> int main() { 
+float c; 
+printf("Name- Meer\nSAP ID - 590028247\nCourse- BCA\nBatch- B5"); 
+	printf("\n 	\n"); 
+printf("Enter value of c : ");
+scanf("%f", &c); float F= (c*(9/5)) + 32; 
+  printf("The temp in fahrenhiet is %f", F);
+  return 0; 
+} 
