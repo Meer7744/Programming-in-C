61f0270871ae92763b7e05594d7c71b73c2b72f2
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:17:27 +0530

    exp3.1.c

---
+++ exp3.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> int main(){ 
+int x; 
+ printf("enter number to be checked"); scanf("%d",Cx); 
+ 
+if (x%2==0){ printf("the number is even"); 
+} 
+else 
+{ 
+printf("the number is odd"); 
+} 
+return 0; 
+} 
