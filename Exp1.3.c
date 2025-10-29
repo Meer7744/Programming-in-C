647a842036e4fea7642be561523110174b61906a
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:10:59 +0530

    Exp1.3.c

---
+++ Exp1.3.c
@@ -0,0 +1,12 @@
+include <stdio.h> int main(){ printf ("Name- Meer\nSAP_ID-590028247\nCourse-BCA\nBatch-5"); 
+printf("\n  \n"); 
+int a; 
+int b; 
+int SUM; 
+printf("Enter the value of number 1 "); 
+scanf("%d",Ca); 
+printf("Enter the value of number 2 "); 
+scanf("%d",Cb); SUM=a+b; 
+printf("The sum of %d and %d is: %d\n", a, b, SUM); 
+return 0; 
+} 
