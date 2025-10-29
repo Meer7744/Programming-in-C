545f2fc47e81ce87a4f71bb7eb43ca57ff527416
Meer7744 <meersingh471@gmail.com>
2025-10-29 16:26:30 +0530

    exp6.1.c

---
+++ exp6.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h> int main() {  
+  int i, j;     
+int r = 5;   
+    printf("Name- Meer\nSAP ID - 590028247\nCourse- BCA\nBatch- B5");  
+    printf("\n-----------------\n");     for (i = 1; i <= r; i++) {for (j = 1; j <= i; j++) {             
+printf("%d", i);  
+ }  
+printf("\n");  }  
+ return 0;  
+}  
